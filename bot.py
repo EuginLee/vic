506b81fba5ce237eadd6c8bc38eee309e25ae723
Eugin <Eugin.lee1@gmail.com>
2020-09-23 02:08:25 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -84,10 +84,10 @@ def main():
     dp.add_handler(CommandHandler('woof',bop))
 
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    #dp.add_handler(MessageHandler(Filters.text, echo))
 
     # log all errors
-    dp.add_error_handler(error)
+    #dp.add_error_handler(error)
 
     # Start the Bot
     updater.start_webhook(listen="0.0.0.0",
