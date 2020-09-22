fdf6f303c1b0622e1ea76ac1500604b7eb894141
Eugin <Eugin.lee1@gmail.com>
2020-09-23 01:11:56 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -66,7 +66,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
-    updater.bot.setWebhook('https://agile-basin-44584.herokuapp.com/' + TOKEN)
+    updater.bot.setWebhook('https://stormy-stream-53137.herokuapp.com/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
